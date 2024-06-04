d63000389c9cfc280e7c9d3c56a6cc4f63883ff8
JuliaHFerreira <juliahelena317@gmail.com>
2024-06-04 19:54:14 -0300

    Incluindo Herança e Polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Conteudo;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
 
@@ -7,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//criando cursos
+		//CRIANDO CURSOS
 		Curso curso1 = new Curso();
 		curso1.setTitulo("Curso java");
 		curso1.setDescricao("Descrição curso java");
@@ -17,14 +18,19 @@ public class Main {
 		curso2.setTitulo("Curso js");
 		curso2.setDescricao("Descrição curso js");
 		curso2.setCargaHoraria(4);
-		
-		//criando mentoria
+	
+		//CRIANDO MENTORIA
 		Mentoria mentoria = new Mentoria();
 		mentoria.setTitulo("Mentoria de java");
 		mentoria.setDescricao("Descrição Mentoria java");
 		mentoria.setData(LocalDate.now());
 		
+		//MENTORIA E CURSO COM CONTEUDO - POLIMORFISMO
 		
+		Conteudo conteudoC = new Curso();
+		Conteudo conteudoM = new Mentoria();
+		
+		//ESCREVENDO NA TELA
 		System.out.println(curso1);
 		System.out.println(curso2);
 		System.out.println(mentoria);
